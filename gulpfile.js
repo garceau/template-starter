62a56b7db8abc7b4cd6f11d7c1a997c74de37b24
Tommy Garceau <tdg0119@icloud.com>
2017-08-20 14:39:07 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+"use strict";
+// Load Em Up
 var gulp = require('gulp');
 const chalk = require('chalk');
 var panini = require('panini');
@@ -11,13 +13,14 @@ var imagemin = require('gulp-imagemin');
 var cache = require('gulp-cache');
 var sketch = require('gulp-sketch');
 
+// SERVE
 gulp.task('serve', ['sass'], function() {
     browserSync.init({
         server: "dist"
     });
 });
 
-
+// IMAGES
 gulp.task('images', function(){
   return gulp.src('src/assets/img/**/*.+(png|jpg|jpeg|gif|svg)')
   .pipe(cache(imagemin({
@@ -26,21 +29,31 @@ gulp.task('images', function(){
   .pipe(gulp.dest('dist/assets/img'))
 });
 
+// FONTS
 gulp.task('fonts', function() {
   return gulp.src('src/assets/fonts/**/*')
   .pipe(gulp.dest('dist/assets/fonts'))
 })
 
+// SCRIPTS:MAIN
 gulp.task('scripts:main', function() {
   return gulp.src('src/assets/js/**/*')
   .pipe(gulp.dest('dist/assets/js'))
 })
 
+// SCRIPTS:VENDOR
 gulp.task('scripts:vendor', function() {
-  return gulp.src('src/assets/js/vendor/**/*')
+  return gulp.src('`src/assets/js/vendor/**/*')
   .pipe(gulp.dest('dist/assets/js/vendor/'))
 })
 
+// STYLES:VENDOR
+gulp.task('styles:vendor', function() {
+  return gulp.src('`src/assets/css/vendor/**/*')
+  .pipe(gulp.dest('dist/assets/css/vendor/'))
+})
+
+// SASS
 gulp.task('sass', function () {
   return gulp.src('./src/assets/scss/**/*.scss')
     .pipe(sass({ 'outputStyle': 'compact'}))
@@ -50,45 +63,51 @@ gulp.task('sass', function () {
     }));
 });
 
+// PANINI
 gulp.task('panini', function() {
-  gulp.src('src/pages/**/*.html')
+  gulp.src('./src/pages/**/*.html')
     .pipe(panini({
-      root: 'src/pages/',
-      layouts: 'src/layouts/',
-      partials: 'src/partials/',
-      helpers: 'src/helpers/',
-      data: 'src/data/'
+      root: './src/pages/',
+      layouts: './src/layouts/',
+      partials: './src/partials/',
+      helpers: './src/helpers/',
+      data: './src/data/'
     }))
     .pipe(gulp.dest('dist'));
 });
 
+// CLEAN: DIST
 gulp.task('clean:dist', function() {
   return del.sync('dist');
 });
 
-
+// CLEAN: JS
 gulp.task('clean:js', function() {
   return del.sync('dist/*.js');
 })
 
+// WATCH FOR CHANGES
 gulp.task('watch', function() {
   gulp.watch(['src/{pages,layouts,partials,helpers,data}/**/*'], ['panini', panini.refresh]);
   gulp.watch('src/assets/scss/**/*.scss', ['sass']);
   gulp.watch('dist/**/*.html').on('change', browserSync.reload);
   gulp.watch('src/assets/js/**/*.js', function() {runSequence('clean:js','scripts:main', 'scripts:vendor')});
   gulp.watch('src/assets/fonts/**/*', ['fonts']);
+  gulp.watch('src/assets/css/**/*', ['styles:vendor']);
   gulp.watch('src/assets/img/**/*.+(png|jpg|jpeg|gif|svg)', ['images']);
 });
 
+// DEFAULT TASK
 gulp.task('default', function(cb) {
-  runSequence(['clean:dist'], ['scripts:main', 'scripts:vendor'], ['panini', 'sass', 'images', 'fonts', 'sketch'], 'serve', 'watch', cb);
+  runSequence(['clean:dist'], ['scripts:main', 'scripts:vendor','styles:vendor'], ['panini', 'sass', 'images', 'fonts', 'sketch'], 'serve', 'watch', cb);
 });
 
+// BUILD TASK
 gulp.task('build', function(cb) {
-  runSequence(['clean:dist', 'clean:data'], ['scripts:main', 'scripts:vendor'], ['panini', 'sass', 'images', 'fonts', 'sketch'], cb);
+  runSequence(['clean:dist', 'clean:data'], ['scripts:main', 'styles:vendor', 'scripts:vendor'], ['panini', 'sass', 'images', 'fonts', 'sketch'], cb);
 });
 
-// SKETCH SLICES: to PNG
+// SKETCH:  SLICES TO PNG
 gulp.task('sketch', function () {
     return gulp.src('src/assets/design/*.sketch')
         .pipe(sketch({
@@ -96,5 +115,5 @@ gulp.task('sketch', function () {
             formats: 'png'
         }))
         .pipe(gulp.dest('dist/assets/img/'));
-        
+ 
 });
